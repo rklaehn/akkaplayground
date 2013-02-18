16ec4794670bcd64eed9d552c6b83f69f335f7a4
Ruediger Klaehn <rklaehn@gmail.com>
2013-02-18 21:04:46 +0100

    Remove printf debugging

--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -10,39 +10,25 @@ case class Print(s: String)
 case object PrintAck
 
 class InverseCalculator(dispatcher:ActorRef) extends Actor {
-  println("InverseCalculator instance created")
 
-  override def preStart() {
-    println("Sending WorkerAvailable to dispatcher")
-    dispatcher ! MessageDiscarder.WorkerAvailable
-  }
+  dispatcher ! MessageDiscarder.WorkerAvailable
 
   def receive = {
     case value:Double =>
       Thread.sleep(1000)
       val result = 1.0/value
       println(result)
-      println("Calculation completed. Make ourselves available again!")
+      // Calculation completed. Make ourselves available again!
       dispatcher ! MessageDiscarder.WorkerAvailable
     case msg =>
-      println("Message not understood. Make ourselves available anyway!")
+      // Message not understood. Make ourselves available anyway!
       dispatcher ! MessageDiscarder.WorkerAvailable
-  }
-}
-
-class Printer extends Actor {
-  def receive = {
-    case Print(s) => {
-      println(s)
-      sender ! PrintAck
-    }
   }
 }
 
 object HelloWorld {
   def main(args: Array[String]) {
     val system = ActorSystem("HelloWorld")
-    val printer = system.actorOf(Props[Printer])
     val discarder = system.actorOf(Props[MessageDiscarder])
     val worker = system.actorOf(Props(new InverseCalculator(discarder)))
     for(i<-1 to 10) {
